239db312623132907b9f0b5d99ff005641c40f2b
MonsieurAmengood2 <zelock@outlook.pt>
2025-01-25 13:37:57 +0000

    Salvar alterações antes de trocar para master

--- app/src/main/java/com/example/funlounge/fragmentos/MenuFragment.kt
+++ app/src/main/java/com/example/funlounge/fragmentos/MenuFragment.kt
@@ -22,8 +22,8 @@ class MenuFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            var param1 = it.getString(ARG_PARAM1)
+            var param2 = it.getString(ARG_PARAM2)
         }
     }
 
